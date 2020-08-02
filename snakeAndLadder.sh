9c046f70a02366070745b5e54040fe4a931f0032
Ram Yadav <ramyadav.now@gmail.com>
2020-08-02 11:27:04 +0530

    Refractor: SnakeAndLadder Program: UserCase01

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 #Initialising Player in Array
-player=( 'player01' 'player02' )
+player=( 'player01' )
 
 for (( i=0; i < ${#player[@]}; i++ ))
 do
    playerPosition[${player[i]}]=0
-
-   echo "${player[i]} is at position ${playerPosition[@]}"
 done
 
