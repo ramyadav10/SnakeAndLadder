4e51f8591c3c2be8c3495943ddf59af962dfcbd9
Ram Yadav <ramyadav.now@gmail.com>
2020-08-02 11:28:49 +0530

    Refractor:Added SnakeAndLadder Program: UserCase02

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 #Initialising Player in Array
-player=( 'player01' 'player02' )
+player=( 'player01' )
 
 for (( i=0; i < ${#player[@]}; i++ ))
 do
    playerPosition[${player[i]}]=0
-
 #Rolling dice to move the player in game
    diceRoll=$(( RANDOM % 6 + 1 ))
-   playerPosition[${player[i]}]=$(( ${playerPostion[${player[i]}]} + $diceRoll >
-   echo "${player[i]} is at position ${playerPosition[@]}"
+   playerPosition[${player[i]}]=$(( ${playerPostion[${player[i]}]} + $diceRoll ))
 done
-
